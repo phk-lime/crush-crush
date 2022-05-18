46902565721fc104cfe1a26e7183705f42341d4e
phk-lime <phk@limefriends.com>
2022-05-18 10:16:13 +0900

    올린다

--- FrameBounds/FrameBounds/ViewController.swift
+++ FrameBounds/FrameBounds/ViewController.swift
@@ -10,11 +10,12 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var view1: UIView!
     @IBOutlet weak var view2: UIView!
+    @IBOutlet weak var view3: UIView!
     
     override func viewDidLoad() {
         view1.bounds.origin.x = 100
         view2.bounds.origin.x = 10
-//        view1.clipsToBounds = true
+        view3.frame.origin.y = 100
     }
 }
 
